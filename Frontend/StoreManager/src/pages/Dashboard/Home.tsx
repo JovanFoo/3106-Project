c047d690a7000d1ddf8992080ee7d06f1c950573
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-03-02 21:00:16 +0800

    add authentication check to Home component for redirecting unauthenticated users

--- Frontend/StoreManager/src/pages/Dashboard/Home.tsx
+++ Frontend/StoreManager/src/pages/Dashboard/Home.tsx
@@ -5,12 +5,16 @@ import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
 import RecentOrders from "../../components/ecommerce/RecentOrders";
 import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
+import { Navigate } from "react-router-dom";
 
 export default function Home() {
+  if (!sessionStorage.getItem("token")) {
+    return <Navigate to="/signin" />;
+  }
   return (
     <>
       <PageMeta
-        title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
+        title="QB House Store Manager Dashboard"
         description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
       <div className="grid grid-cols-12 gap-4 md:gap-6">
