90bedea9f237d56eeb95819a4ee2e8dae36b1e2d
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-03-10 23:06:16 +0800

    Update server port configuration to use environment variable

--- Backend/index.js
+++ Backend/index.js
@@ -31,6 +31,6 @@ app.use(express.static("public"));
 
 app.use("/api", ApiRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
 });
