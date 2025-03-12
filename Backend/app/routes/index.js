809c47eb4318014b673a56835940c47a7885d0a8
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-03-13 00:29:04 +0800

    Update index.js

--- Backend/app/routes/index.js
+++ Backend/app/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ApiRouter = express.Router();
 
-const CustomerRouter = require("./customerRoute.js");
+const CustomerRouter = require("./CustomerRoute.js");
 const AuthRouter = require("./AuthRoute.js");
 
 ApiRouter.use("/customers", CustomerRouter);
