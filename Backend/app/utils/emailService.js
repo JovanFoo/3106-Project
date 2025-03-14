56ebf6218028efd6f501da975ea32c643d4400af
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-03-15 02:37:58 +0800

    Update emailService.js

--- Backend/app/utils/emailService.js
+++ Backend/app/utils/emailService.js
@@ -22,8 +22,10 @@ const resetPassword = async (to, name, token) => {
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
+      return false;
     } else {
       console.log("Email sent: " + info.response);
+      return true;
     }
   });
 };
