f3b41d210669b676a6595f7b5f1340f1dfc4297b
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-03-17 21:12:32 +0800

    updated readme

--- Backend/app/routes/index.js
+++ Backend/app/routes/index.js
@@ -8,19 +8,22 @@ const BranchRoute = require("./BranchRoute.js");
 const CustomerRouter = require("./CustomerRoute.js");
 const PromotionRouter = require("./PromotionRoute.js");
 // const PaymentRouter = require("./PaymentRouter.js");
-// const ReviewRouter = require("./AppointmentRoute.js");
+const ReviewRouter = require("./ReviewRoute.js");
 const ExpertiseRouter = require("./ExpertiseRoute.js");
 const StylistRouter = require("./StylistRoute.js");
+// const DiscountRouter = require("./DiscountRoute.js");
+const ServiceRouter = require("./ServiceRoute.js");
 
 ApiRouter.use("/customers", CustomerRouter);
 ApiRouter.use("/auth", AuthRouter);
-ApiRouter.use("/Appointments", AppointmentRouter);
+ApiRouter.use("/appointments", AppointmentRouter);
 // ApiRouter.use("/payments", PaymentRouter);
+ApiRouter.use("/reviews", ReviewRouter);
 ApiRouter.use("/branches", BranchRoute);
+
 ApiRouter.use("/promotions", PromotionRouter);
-// ApiRouter.use("/reviews", CustomerRouter);
-// ApiRouter.use("/discounts", CustomerRouter);
-// ApiRouter.use("/services", CustomerRouter);
+// ApiRouter.use("/discounts", DiscountRouter);
+ApiRouter.use("/services", ServiceRouter);
 ApiRouter.use("/stylists", StylistRouter);
 ApiRouter.use("/expertises", ExpertiseRouter);
 
