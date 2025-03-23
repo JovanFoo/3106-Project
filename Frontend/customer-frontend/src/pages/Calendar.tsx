8bd8255e22cb25bf6232fb3fcfa709bc430a7ae8
yewkhang <yewkhang45@outlook.com>
2025-03-24 01:49:39 +0800

    updated appointments page to receive the needed info

--- Frontend/customer-frontend/src/pages/Calendar.tsx
+++ Frontend/customer-frontend/src/pages/Calendar.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
@@ -14,7 +14,8 @@ import Button from "../components/ui/button/Button";
 
 interface CalendarEvent extends EventInput {
   extendedProps: {
-    calendar: string;
+    request: string;
+    service: string;
   };
 }
 
@@ -22,100 +23,79 @@ const Calendar: React.FC = () => {
   const [selectedEvent, setSelectedEvent] = useState<CalendarEvent | null>(
     null
   );
-  const [eventTitle, setEventTitle] = useState("");
-  const [eventStartDate, setEventStartDate] = useState("");
-  const [eventEndDate, setEventEndDate] = useState("");
-  const [eventLevel, setEventLevel] = useState("");
-  const [events, setEvents] = useState<CalendarEvent[]>([]);
+  const [apptName, setApptName] = useState("");
+  const [apptStartDate, setApptStartDate] = useState("");
+  const [apptEndDate, setApptEndDate] = useState("");
+  const [request, setRequest] = useState("");
+  const [service, setService] = useState("");
+  const [appt, setAppts] = useState<CalendarEvent[]>([]);
   const calendarRef = useRef<FullCalendar>(null);
   const { isOpen, openModal, closeModal } = useModal();
 
-  const calendarsEvents = {
-    Danger: "danger",
-    Success: "success",
-    Primary: "primary",
-    Warning: "warning",
-  };
-
-  useEffect(() => {
-    // Initialize with some events
-    setEvents([
-      {
-        id: "1",
-        title: "Event Conf.",
-        start: new Date().toISOString().split("T")[0],
-        extendedProps: { calendar: "Danger" },
-      },
-      {
-        id: "2",
-        title: "Meeting",
-        start: new Date(Date.now() + 86400000).toISOString().split("T")[0],
-        extendedProps: { calendar: "Success" },
-      },
-      {
-        id: "3",
-        title: "Workshop",
-        start: new Date(Date.now() + 172800000).toISOString().split("T")[0],
-        end: new Date(Date.now() + 259200000).toISOString().split("T")[0],
-        extendedProps: { calendar: "Primary" },
-      },
-    ]);
-  }, []);
+  // const calendarsEvents = {
+  //   Danger: "danger",
+  //   Success: "success",
+  //   Primary: "primary",
+  //   Warning: "warning",
+  // };
 
   const handleDateSelect = (selectInfo: DateSelectArg) => {
     resetModalFields();
-    setEventStartDate(selectInfo.startStr);
-    setEventEndDate(selectInfo.endStr || selectInfo.startStr);
+    setApptStartDate(selectInfo.startStr);
+    setApptEndDate(selectInfo.endStr || selectInfo.startStr);
     openModal();
   };
 
   const handleEventClick = (clickInfo: EventClickArg) => {
-    const event = clickInfo.event;
-    setSelectedEvent(event as unknown as CalendarEvent);
-    setEventTitle(event.title);
-    setEventStartDate(event.start?.toISOString().split("T")[0] || "");
-    setEventEndDate(event.end?.toISOString().split("T")[0] || "");
-    setEventLevel(event.extendedProps.calendar);
+    const appt = clickInfo.event;
+    setSelectedEvent(appt as unknown as CalendarEvent);
+    setApptName(appt.title);
+    setRequest(appt.extendedProps.request);
+    setService(appt.extendedProps.service);
+    setApptStartDate(appt.start?.toISOString().split("T")[0] || "");
+    setApptEndDate(appt.end?.toISOString().split("T")[0] || "");
     openModal();
   };
 
   const handleAddOrUpdateEvent = () => {
     if (selectedEvent) {
       // Update existing event
-      setEvents((prevEvents) =>
+      setAppts((prevEvents) =>
         prevEvents.map((event) =>
           event.id === selectedEvent.id
             ? {
                 ...event,
-                title: eventTitle,
-                start: eventStartDate,
-                end: eventEndDate,
-                extendedProps: { calendar: eventLevel },
+                title: apptName,
+                start: apptStartDate,
+                end: apptEndDate,
+                extendedProps: { service: service, request: request },
               }
             : event
         )
       );
     } else {
-      // Add new event
-      const newEvent: CalendarEvent = {
+      // Add new appointment
+      const newAppointment: CalendarEvent = {
         id: Date.now().toString(),
-        title: eventTitle,
-        start: eventStartDate,
-        end: eventEndDate,
-        allDay: true,
-        extendedProps: { calendar: eventLevel },
+        title: apptName,
+        start: apptStartDate,
+        end: apptEndDate,
+        allDay: true, // change to set the timing
+        extendedProps: { request: request, service: service },
       };
-      setEvents((prevEvents) => [...prevEvents, newEvent]);
+      setAppts((prevEvents) => [...prevEvents, newAppointment]);
     }
     closeModal();
     resetModalFields();
   };
 
   const resetModalFields = () => {
-    setEventTitle("");
-    setEventStartDate("");
-    setEventEndDate("");
-    setEventLevel("");
+    setApptName("");
+    setRequest("");
+    setService("");
+    setApptStartDate("");
+    setApptEndDate("");
+    // setEventLevel("");
     setSelectedEvent(null);
   };
 
@@ -137,14 +117,14 @@ const Calendar: React.FC = () => {
               center: "title",
               right: "dayGridMonth,timeGridWeek,timeGridDay",
             }}
-            events={events}
+            events={appt}
             selectable={true}
             select={handleDateSelect}
             eventClick={handleEventClick}
             eventContent={renderEventContent}
             customButtons={{
               addEventButton: {
-                text: "Book an Appointment +",
+                text: "Book an Appointment",
                 click: openModal,
               },
             }}
@@ -158,29 +138,28 @@ const Calendar: React.FC = () => {
           <div className="flex flex-col px-2 overflow-y-auto custom-scrollbar">
             <div>
               <h5 className="mb-2 font-semibold text-gray-800 modal-title text-theme-xl dark:text-white/90 lg:text-2xl">
-                {selectedEvent ? "Edit Event" : "Add Event"}
+                {selectedEvent ? "Edit Appointment" : "Add Appointment"}
               </h5>
               <p className="text-sm text-gray-500 dark:text-gray-400">
-                Plan your next big moment: schedule or edit an event to stay on
-                track
+                Book an Appointment Now!
               </p>
             </div>
             <div className="mt-8">
               <div>
                 <div>
                   <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
-                    Event Title
+                    Appointment Name
                   </label>
                   <input
-                    id="event-title"
+                    id="title"
                     type="text"
-                    value={eventTitle}
-                    onChange={(e) => setEventTitle(e.target.value)}
+                    value={apptName}
+                    onChange={(e) => setApptName(e.target.value)}
                     className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-none focus:ring focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
                   />
                 </div>
               </div>
-              <div className="mt-6">
+              {/* <div className="mt-6">
                 <label className="block mb-4 text-sm font-medium text-gray-700 dark:text-gray-400">
                   Event Color
                 </label>
@@ -214,7 +193,7 @@ const Calendar: React.FC = () => {
                     </div>
                   ))}
                 </div>
-              </div>
+              </div> */}
 
               <div className="mt-6">
                 <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
@@ -224,8 +203,8 @@ const Calendar: React.FC = () => {
                   <input
                     id="event-start-date"
                     type="date"
-                    value={eventStartDate}
-                    onChange={(e) => setEventStartDate(e.target.value)}
+                    value={apptStartDate}
+                    onChange={(e) => setApptStartDate(e.target.value)}
                     className="dark:bg-dark-900 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pl-4 pr-11 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-none focus:ring focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
                   />
                 </div>
@@ -239,11 +218,35 @@ const Calendar: React.FC = () => {
                   <input
                     id="event-end-date"
                     type="date"
-                    value={eventEndDate}
-                    onChange={(e) => setEventEndDate(e.target.value)}
+                    value={apptEndDate}
+                    onChange={(e) => setApptEndDate(e.target.value)}
                     className="dark:bg-dark-900 h-11 w-full appearance-none rounded-lg border border-gray-300 bg-transparent bg-none px-4 py-2.5 pl-4 pr-11 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-none focus:ring focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
                   />
                 </div>
+              </div>
+              <div className="mt-6">
+                <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                  Service
+                </label>
+                <input
+                  id="location"
+                  type="text"
+                  value={service}
+                  onChange={(e) => setService(e.target.value)}
+                  className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-none focus:ring focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                />
+              </div>
+              <div className="mt-6">
+                <label className="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                  Request
+                </label>
+                <input
+                  id="request"
+                  type="text"
+                  value={request}
+                  onChange={(e) => setRequest(e.target.value)}
+                  className="dark:bg-dark-900 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 shadow-theme-xs placeholder:text-gray-400 focus:border-brand-300 focus:outline-none focus:ring focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800"
+                />
               </div>
             </div>
             <div className="flex items-center gap-3 mt-6 modal-footer sm:justify-end">
@@ -259,7 +262,7 @@ const Calendar: React.FC = () => {
                 type="button"
                 className="btn btn-success btn-update-event flex w-full justify-center rounded-lg bg-brand-500 px-4 py-2.5 text-sm font-medium text-white hover:bg-brand-600 sm:w-auto"
               >
-                {selectedEvent ? "Update Changes" : "Add Event"}
+                {selectedEvent ? "Update Appointment" : "Book Appointment"}
               </button>
             </div>
           </div>
@@ -276,10 +279,9 @@ const Calendar: React.FC = () => {
 };
 
 const renderEventContent = (eventInfo: any) => {
-  const colorClass = `fc-bg-${eventInfo.event.extendedProps.calendar.toLowerCase()}`;
   return (
     <div
-      className={`event-fc-color flex fc-event-main ${colorClass} p-1 rounded`}
+      className={`event-fc-color flex fc-event-main fc-bg-primary p-1 rounded`}
     >
       <div className="fc-daygrid-event-dot"></div>
       <div className="fc-event-time">{eventInfo.timeText}</div>
