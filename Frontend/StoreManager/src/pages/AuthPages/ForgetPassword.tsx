620616557145c6f7ceb1a889b437e8b5b36d2728
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-03-25 14:12:29 +0800

    feat: add Forget Password page with layout and form integration

---
+++ Frontend/StoreManager/src/pages/AuthPages/ForgetPassword.tsx
@@ -0,0 +1,17 @@
+import PageMeta from "../../components/common/PageMeta";
+import AuthLayout from "./AuthPageLayout";
+import ForgetPasswordForm from "../../components/auth/ForgetPasswordForm";
+
+export default function SignIn() {
+  return (
+    <>
+      <PageMeta
+        title="QB House Store Manager - Forget Password"
+        description="This is a Dashboard page for QB House Store Manager Sign in Page"
+      />
+      <AuthLayout>
+        <ForgetPasswordForm />
+      </AuthLayout>
+    </>
+  );
+}
