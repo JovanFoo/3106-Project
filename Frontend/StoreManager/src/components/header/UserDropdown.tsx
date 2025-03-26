920ea090254bf3a53ae8772ef0aceb0b70808023
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-03-26 19:25:33 +0800

    feat: enhance UserDropdown component with user data fetching and dynamic display; update profile picture and email

--- Frontend/StoreManager/src/components/header/UserDropdown.tsx
+++ Frontend/StoreManager/src/components/header/UserDropdown.tsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DropdownItem } from "../ui/dropdown/DropdownItem";
 import { Dropdown } from "../ui/dropdown/Dropdown";
 import { Link } from "react-router";
+import axios, { AxiosResponse } from "axios";
+
+const api_address = import.meta.env.VITE_APP_API_ADDRESS_PROD;
+// const api_address = import.meta.env.VITE_APP_API_ADDRESS_DEV;
+
+const config = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    Authorization: sessionStorage.getItem("token"),
+  },
+};
 
 export default function UserDropdown() {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [username, setUsername] = useState("Username");
+  const [email, setEmail] = useState("example@email.com");
+  const [profilePicture, setProfilePicture] = useState("/images/user/owner.jpg");
   function toggleDropdown() {
     setIsOpen(!isOpen);
   }
@@ -13,6 +27,34 @@ export default function UserDropdown() {
   function closeDropdown() {
     setIsOpen(false);
   }
+  function handleSignout() {
+    sessionStorage.clear();
+    window.location.href = "/signin";
+  }
+
+  useEffect(() => {
+    const selfId =
+      sessionStorage.getItem("userId") || "67dd2e03c46b39e1f555a317";
+    const fetchData = async () => {
+      await axios
+        .get(api_address + "/api/stylists/" + selfId, config)
+        .then((res: AxiosResponse) => {
+          if (res.status !== 200) {
+            console.log("Failed to fetch user data.");
+            return;
+          }
+          setUsername(res.data.username);
+          setProfilePicture(
+            res.data.profilePicture || "/images/user/owner.jpg"
+          );
+          setEmail(res.data.email);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    fetchData();
+  }, []);
   return (
     <div className="relative">
       <button
@@ -20,10 +62,10 @@ export default function UserDropdown() {
         className="flex items-center text-gray-700 dropdown-toggle dark:text-gray-400"
       >
         <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-          <img src="/images/user/owner.jpg" alt="User" />
+          <img src={profilePicture} />
         </span>
 
-        <span className="block mr-1 font-medium text-theme-sm">Musharof</span>
+        <span className="block mr-1 font-medium text-theme-sm">{username}</span>
         <svg
           className={`stroke-gray-500 dark:stroke-gray-400 transition-transform duration-200 ${
             isOpen ? "rotate-180" : ""
@@ -51,10 +93,10 @@ export default function UserDropdown() {
       >
         <div>
           <span className="block font-medium text-gray-700 text-theme-sm dark:text-gray-400">
-            Musharof Chowdhury
+            {username}
           </span>
           <span className="mt-0.5 block text-theme-xs text-gray-500 dark:text-gray-400">
-            randomuser@pimjo.com
+            {email}
           </span>
         </div>
 
@@ -63,7 +105,7 @@ export default function UserDropdown() {
             <DropdownItem
               onItemClick={closeDropdown}
               tag="a"
-              to="/profile"
+              to="settings/profile"
               className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
             >
               <svg
@@ -88,7 +130,7 @@ export default function UserDropdown() {
             <DropdownItem
               onItemClick={closeDropdown}
               tag="a"
-              to="/profile"
+              to="/settings/notifications"
               className="flex items-center gap-3 px-3 py-2 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
             >
               <svg
@@ -109,7 +151,7 @@ export default function UserDropdown() {
               Account settings
             </DropdownItem>
           </li>
-          <li>
+          {/* <li>
             <DropdownItem
               onItemClick={closeDropdown}
               tag="a"
@@ -133,9 +175,10 @@ export default function UserDropdown() {
               </svg>
               Support
             </DropdownItem>
-          </li>
+          </li> */}
         </ul>
         <Link
+          onClick={handleSignout}
           to="/signin"
           className="flex items-center gap-3 px-3 py-2 mt-3 font-medium text-gray-700 rounded-lg group text-theme-sm hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-white/5 dark:hover:text-gray-300"
         >
