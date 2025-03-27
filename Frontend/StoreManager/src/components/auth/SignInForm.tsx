72b721778b7d1f6f5cc84bc1d4e3a555caecd319
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-03-27 15:23:17 +0800

    fix: switch API address to development environment for SignInForm

--- Frontend/StoreManager/src/components/auth/SignInForm.tsx
+++ Frontend/StoreManager/src/components/auth/SignInForm.tsx
@@ -8,10 +8,9 @@ import Checkbox from "../form/input/Checkbox";
 import Button from "../ui/button/Button";
 import { useNavigate } from "react-router-dom";
 import Alert from "../ui/alert/Alert";
-import { set } from "date-fns";
 
-const api_address = import.meta.env.VITE_APP_API_ADDRESS_PROD;
-// const api_address = import.meta.env.VITE_APP_API_ADDRESS_DEV;
+// const api_address = import.meta.env.VITE_APP_API_ADDRESS_PROD;
+const api_address = import.meta.env.VITE_APP_API_ADDRESS_DEV;
 
 export default function SignInForm() {
   const [showPassword, setShowPassword] = useState(false);
