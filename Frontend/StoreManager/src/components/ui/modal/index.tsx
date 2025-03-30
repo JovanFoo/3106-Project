5dfbe16af0568f43d37ceaf6cadf30dc5d76c235
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-03-30 15:08:58 +0800

    fix: remove unnecessary line break in Modal component

--- Frontend/StoreManager/src/components/ui/modal/index.tsx
+++ Frontend/StoreManager/src/components/ui/modal/index.tsx
@@ -67,7 +67,6 @@ export const Modal: React.FC<ModalProps> = ({
         onClick={(e) => e.stopPropagation()}
       >
         {showCloseButton && (
-
           <button
             onClick={onClose}
             className="absolute right-3 top-3 z-999 flex h-9.5 w-9.5 items-center justify-center rounded-full bg-gray-100 text-gray-400 transition-colors hover:bg-gray-200 hover:text-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white sm:right-6 sm:top-6 sm:h-11 sm:w-11"
@@ -88,7 +87,7 @@ export const Modal: React.FC<ModalProps> = ({
             </svg>
           </button>
         )}
-        <div>{children}</div>
+        {children}
       </div>
     </div>
   );
