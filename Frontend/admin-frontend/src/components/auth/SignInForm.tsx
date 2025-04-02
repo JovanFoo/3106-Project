2bdedcac8e098a1add0de0187ed5bf5233488e66
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-04-03 00:49:04 +0800

    remove signup form

--- Frontend/admin-frontend/src/components/auth/SignInForm.tsx
+++ Frontend/admin-frontend/src/components/auth/SignInForm.tsx
@@ -155,7 +155,7 @@ export default function SignInForm() {
             <div className={showAlert ? " mt-5" : "mt-5 hidden"}>
               <Alert variant={variant} title={title} message={message} />
             </div>
-            <div className="mt-5">
+            {/* <div className="mt-5">
               <p className="text-sm font-normal text-center text-gray-700 dark:text-gray-400 sm:text-start">
                 Don&apos;t have an account? {""}
                 <Link
@@ -165,7 +165,7 @@ export default function SignInForm() {
                   Sign Up
                 </Link>
               </p>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
