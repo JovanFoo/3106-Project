4863c7fb4fe22d1952eb308ef0e4bf1f7db6a740
Jett-Tan <68832631+Jett-Tan@users.noreply.github.com>
2025-04-17 02:46:52 +0800

    Update TeamsController.js

--- Backend/app/controllers/TeamsController.js
+++ Backend/app/controllers/TeamsController.js
@@ -88,9 +88,9 @@ const TeamController = {
         .status(400)
         .json({ message: "You are not the manager of this branch" });
     }
-    if (stylistManagerId == stylistId) {
-      return res.status(400).json({ message: "You cannot add yourself" });
-    }
+    // if (stylistManagerId == stylistId) {
+    //   return res.status(400).json({ message: "You cannot add yourself" });
+    // }
     // add stylist to the branch and stylist manager
     let included = false;
     if (branch.stylists.includes(stylistId)) {
