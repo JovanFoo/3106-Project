7e969ed1aadf6cab87246a0cd92cf6481f3a8468
andre <lautseernandre1@gmail.com>
2025-04-16 19:52:34 +0800

    changes

--- Frontend/customer-frontend/src/pages/Tables/ApptTable.tsx
+++ Frontend/customer-frontend/src/pages/Tables/ApptTable.tsx
@@ -56,10 +56,7 @@ interface Props {
 export default function ApptTable({ appointment }: Props) {
   return (
     <>
-      <PageMeta
-        title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
-      />
+      <PageMeta title="BuzzBook" description="BuzzBook" />
       {/* <PageBreadcrumb pageTitle="Appointments" /> */}
       <div className="space-y-6">
         <AppointmentTableOne appointment={appointment} />
--- Frontend/customer-frontend/src/pages/Tables/BasicTables2.tsx
+++ Frontend/customer-frontend/src/pages/Tables/BasicTables2.tsx
@@ -9,10 +9,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 export default function BasicTables2() {
   return (
     <>
-      <PageMeta
-        title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
-        description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
-      />
+      <PageMeta title="BuzzBook" description="BuzzBooke" />
       <PageBreadcrumb pageTitle="Our Team" />
       <div className="space-y-6">
         <ComponentCard title="Our Stylists">
