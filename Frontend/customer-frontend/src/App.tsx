34469719f822c32a80a6527b13b5590c37b9427d
yewkhang <yewkhang45@outlook.com>
2025-04-19 00:19:27 +0800

    cleaning up

--- Frontend/customer-frontend/src/App.tsx
+++ Frontend/customer-frontend/src/App.tsx
@@ -9,8 +9,6 @@ import Alerts from "./pages/UiElements/Alerts";
 import Badges from "./pages/UiElements/Badges";
 import Avatars from "./pages/UiElements/Avatars";
 import Buttons from "./pages/UiElements/Buttons";
-import LineChart from "./pages/Charts/LineChart";
-import BarChart from "./pages/Charts/BarChart";
 import Calendar from "./pages/Calendar";
 import FormElements from "./pages/Forms/FormElements";
 import Blank from "./pages/Blank";
@@ -45,30 +43,15 @@ export default function App() {
             <Route index path="/" element={<Navigate to="/signin" />} />
             <Route index path="/home" element={<Navigate to="/appointments" />} />
 
-            {/* Others Page */}
+            {/* Main Pages */}
             <Route path="/appointments" element={<Calendar />} />
             <Route path="/viewappointments" element={<PastApptsPage />} />
             <Route path="/profile" element={<UserProfiles />} />
             <Route path="/reviews" element={<ReviewsList />} />
             <Route path="/blank" element={<Blank />} />
 
-            {/* Forms */}
-            <Route path="/form-elements" element={<FormElements />} />
-
             {/* Tables */}
             <Route path="/allstylists" element={<BasicTables />} />
-
-            {/* Ui Elements */}
-            <Route path="/alerts" element={<Alerts />} />
-            <Route path="/avatars" element={<Avatars />} />
-            <Route path="/badge" element={<Badges />} />
-            <Route path="/buttons" element={<Buttons />} />
-            <Route path="/images" element={<Images />} />
-            <Route path="/videos" element={<Videos />} />
-
-            {/* Charts */}
-            <Route path="/line-chart" element={<LineChart />} />
-            <Route path="/bar-chart" element={<BarChart />} />
           </Route>
 
           {/* Auth Layout */}
