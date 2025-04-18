012bfc7fc9344b0f196207b9b8c077aa263f07cd
wallace <wallacephua26@gmail.com>
2025-04-18 22:29:48 +0800

    updated UI for profile picture on top-right

--- Frontend/customer-frontend/src/components/header/UserDropdown.tsx
+++ Frontend/customer-frontend/src/components/header/UserDropdown.tsx
@@ -59,13 +59,12 @@ export default function UserDropdown() {
         onClick={toggleDropdown}
         className="flex items-center text-gray-700 dropdown-toggle dark:text-gray-400"
       >
-        <span className="mr-3 overflow-hidden rounded-full h-11 w-11">
-          <div>
-            <img
-              src={profilepic || "/images/logo/defaultprofile.png"}
-              alt="user"
-            />
-          </div>
+        <span className="mr-3 h-11 w-11 overflow-hidden rounded-full">
+          <img
+            src={profilepic || "/images/logo/defaultprofile.png"}
+            alt="user"
+            className="object-cover h-full w-full"
+          />
         </span>
 
         <span className="block mr-1 font-medium text-theme-sm">{name}</span>
